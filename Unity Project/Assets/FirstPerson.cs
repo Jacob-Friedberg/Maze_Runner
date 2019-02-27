b997fcfa82e61e93551d98252c3c6b344b7ffac2
hallmanben <hallmanben@gmail.com>
2019-02-26 17:34:27 -0800

    FPS Controls

---
+++ Unity Project/Assets/FirstPerson.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    public float speed = 20f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+        Vector3 targetDirection = transform.position;
+        targetDirection = Camera.main.transform.TransformDirection(targetDirection);
+        targetDirection.y = 0.0f;
+
+        //Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            targetDirection.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            targetDirection.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            targetDirection.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            targetDirection.x -= speed * Time.deltaTime;
+        }
+        transform.position = targetDirection;
+    }
+}
