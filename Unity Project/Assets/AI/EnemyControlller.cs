c5101368f2b0fd5e78fce0fea040c6bcbd71ff55
Conrad Mearns <conradmearns@gmail.com>
2019-02-28 12:50:05 -0800

    Begining of Game Completion Code

--- Unity Project/Assets/AI/EnemyControlller.cs
+++ Unity Project/Assets/AI/EnemyControlller.cs
@@ -9,8 +9,8 @@ public class EnemyControlller : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0)){
-           Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-           RaycastHit hit;
+          Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+          RaycastHit hit;
 
            if(Physics.Raycast(ray, out hit)){
                // MOVE AGENT
