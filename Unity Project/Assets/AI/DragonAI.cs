d11b04a14ab8151d1af2a5158a29b52a32c6f08e
Conrad Mearns <conradmearns@gmail.com>
2019-02-28 13:29:34 -0800

    Create DragonAI.cs
    Base class for dragon AI behavior

---
+++ Unity Project/Assets/AI/DragonAI.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+public class DragonAI : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public GameObject target;
+    // Update is called once per frame
+
+    void Start()
+    {
+      agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
