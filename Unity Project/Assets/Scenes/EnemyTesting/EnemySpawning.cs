1c3227817391bb4a572d61726ce82f1ded3739ae
Corbono2 <47007310+Corbono2@users.noreply.github.com>
2019-03-19 19:18:17 -0700

    Enemy Stress Testing

---
+++ Unity Project/Assets/Scenes/EnemyTesting/EnemySpawning.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+    public GameObject Dragon;
+    public Vector3 spawnValues;
+    public int startWait;
+    int DragonCount = 1;
+
+
+
+
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+
+            Instantiate(Dragon, spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            DragonCount++;
+            Debug.Log("Dragon Count: " + DragonCount);
+            yield return new WaitForSeconds(0.000001F);
+        }
+    }
+}
