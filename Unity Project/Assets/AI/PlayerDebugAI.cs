464ff099afc578227d58f5badd58862ae236920b
Conrad Mearns <conradmearns@gmail.com>
2019-03-20 10:05:48 -0700

    Update PlayerDebugAI.cs

--- Unity Project/Assets/AI/PlayerDebugAI.cs
+++ Unity Project/Assets/AI/PlayerDebugAI.cs
@@ -7,6 +7,11 @@ public class PlayerDebugAI : MonoBehaviour
 
   public Camera cam;
   public NavMeshAgent agent;
+
+  private int step = 0;
+  public GameObject challice;
+  public GameObject alter;
+
   // Update is called once per frame
   void Update()
   {
@@ -22,7 +27,23 @@ public class PlayerDebugAI : MonoBehaviour
 
       }
     } else {
-      //Automode
+      switch (step) {
+        case 0:
+          agent.SetDestination(challice.transform.position);
+          if(2 > Vector3.Distance(challice.transform.position, transform.position)){
+            step++;
+          }
+        break;
+        case 1:
+          agent.SetDestination(alter.transform.position);
+          if(2 > Vector3.Distance(alter.transform.position, transform.position)){
+            step++;
+          }
+        break;
+        default:
+          Debug.Log("AI AUTO RUN COMPLETE");
+        break;
+      }
     }
   }
 }
