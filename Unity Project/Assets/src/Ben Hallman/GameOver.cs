02ed443798dc554c5542d70464b6002bc3e7bf8d
hallmanben <hallmanben@gmail.com>
2019-03-21 15:10:54 -0700

    Added health

---
+++ Unity Project/Assets/src/Ben Hallman/GameOver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeakness : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("DeathTrigger"))
+        {
+            Debug.Log("PLAYER DEATH");
+        }
+    }
+}
