66035e88f5b9b1dc06d808becee589cf1472cee1
hallmanben <hallmanben@gmail.com>
2019-03-25 13:44:08 -0700

    Update GameOver.cs

--- Unity Project/Assets/src/Ben Hallman/GameOver.cs
+++ Unity Project/Assets/src/Ben Hallman/GameOver.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerWeakness : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
     void OnTriggerEnter(Collider col)
     {
