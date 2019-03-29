ba7aabacb8a37aed69e9edcdbd404a2428218779
Corbono2 <corbin18@live.com>
2019-03-28 18:38:18 -0700

    Adding changes to DragonHead.cs

---
+++ Unity Project/Assets/src/Corbin/DragonHead.cs
@@ -0,0 +1,22 @@
+﻿/* DragonHead.cs
+Corbin and Conrad
+This script is responsible for triggering the TakeDamage function
+when the collider sphere in the dragon's head registers a collision
+with the sword. */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonHead : MonoBehaviour
+{
+  public GameObject dragon;
+  void OnTriggerEnter (Collider col)
+  {
+    if (col.gameObject.CompareTag("Sword"))
+    {
+      dragon.GetComponent<DragonAI>().TakeDamage();
+      yield return new WaitForSeconds(1);
+    }
+  }
+}
