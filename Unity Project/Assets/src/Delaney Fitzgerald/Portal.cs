43ea845fcce7493ce589d006c7cabd339e450f49
Delaney Fitzgerald <Fitz6051@vandals.uidaho.edu>
2019-03-28 18:17:06 -0700

    update portal-controller interaction

---
+++ Unity Project/Assets/src/Delaney Fitzgerald/Portal.cs
@@ -0,0 +1,29 @@
+﻿/* Portal.cs
+   Delaney Fitzgerald
+   class to trigger the change of scene on contact with the player*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter (Collision col)
+    {
+      if (col.gameObject.CompareTag("Control"))
+      {
+        Debug.Log ("Change Scene Now!");
+      }
+    }
+}
