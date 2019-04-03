bfb3cb8a45ecaae68c1dcfed6904c42ae299a629
Conrad Mearns <conradmearns@gmail.com>
2019-04-03 15:15:06 -0700

    Updated

--- Unity Project/Assets/Menus/hit_controller.cs
+++ Unity Project/Assets/Menus/hit_controller.cs
@@ -11,13 +11,41 @@ public class hit_controller : MonoBehaviour
       player = transform.parent.parent.gameObject;
     }
 
+
+
     // Watch for the player to interact with the play button
     void OnTriggerEnter(Collider col)
     {
+      //Start menu to world 1
         if(col.gameObject.CompareTag("StartWorld"))
         {
-            SceneManager.LoadScene("maze_game");
-            player.transform.position = new Vector3(0,0,0);
+            SceneManager.LoadScene("world1");
+            player.transform.position = new Vector3(-103, 0, -43);
         }
+
+        if(col.gameObject.CompareTag("MazeToWorld"))
+        {
+            SceneManager.LoadScene("Proc_gen");
+            player.transform.position = new Vector3(3.75f, 0.5f, 3.75f);
+        }
+
+        if(col.gameObject.CompareTag("ToMazeExit"))
+        {
+            SceneManager.LoadScene("world1");
+            player.transform.position = new Vector3(12, 0, -307);
+        }
+
+        if(col.gameObject.CompareTag("MazeToCastle"))
+        {
+            SceneManager.LoadScene("Proc_gen");
+            player.transform.position = new Vector3(48.75f, 0.5f, 41.75f);
+        }
+
+        if(col.gameObject.CompareTag("ToMazeEntrance"))
+        {
+            SceneManager.LoadScene("world1");
+            player.transform.position = new Vector3(87, 0, -43);
+        }
+
     }
 }
