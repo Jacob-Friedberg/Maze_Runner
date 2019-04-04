c65d8570cb632bcfa4bbc668d402908097a93158
Conrad Mearns <conradmearns@gmail.com>
2019-04-03 17:23:03 -0700

    Tracking Corbin's Changes

--- Unity Project/Assets/src/Corbin/DragonAI.cs
+++ Unity Project/Assets/src/Corbin/DragonAI.cs
@@ -11,21 +11,24 @@ using System.Collections;
 public class DragonAI : MonoBehaviour
 {
   [SerializeField] private float attackDistance;
+  [SerializeField] private GameObject dragon;
+  [SerializeField] private GameObject attackTrigger;
+  [SerializeField] private GameObject target;
+  // difficulty is to be set between 1 - 3 with 1 being Dr. BC mode and 3 being the hardest difficulty
+  public int difficulty;
+  // sceneID is to know which unique dragon to spawn in
+  public int sceneID;
+  private int dragonHealth;
+  private int swordDamage;
   private NavMeshAgent agent;
   private bool isAttacking = false;
   private bool isDead = false;
 
 
-  public GameObject attackTrigger;
-  public GameObject target;
-  // difficulty is to be set between 1 - 3 with 1 being Dr. BC mode and 3 being the hardest difficulty
-  public int difficulty;
-  private int dragonHealth;
-  private int swordDamage;
-
   void Start()
   {
     agent = this.GetComponent<NavMeshAgent>();
+
     // Initialize difficulty settings
     switch (difficulty)
     {
@@ -46,27 +49,64 @@ public class DragonAI : MonoBehaviour
         swordDamage = 100;
         break;
     }
+
   }
 
   void Update()
   {
-    // if (false == isAttacking && false == isDead)
-    // {
-    //   agent.SetDestination(target.transform.position);
-    //
-    //   if (Vector3.Distance(attackTrigger.transform.position, target.transform.position) < attackDistance)
-    //   {
-    //     GetComponent<Animator>().Play("Atk_Claw_DBL", 0, 0f);
-    //     isAttacking = true;
-    //     agent.isStopped = true;
-    //   }
-    // } else if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Walk"))
-    // {
-    //     isAttacking = false;
-    //     agent.isStopped = false;
-    // }
+    if (false == isAttacking && false == isDead)
+    {
+      agent.SetDestination(target.transform.position);
+
+      if (Vector3.Distance(attackTrigger.transform.position, target.transform.position) < attackDistance)
+      {
+        switch ((int)Random.Range(0.0f, 2.99f))
+        {
+            case 0:
+            GetComponent<Animator>().Play("Atk_Claw_DBL", 0, 0f);
+            break;
+            case 1:
+            GetComponent<Animator>().Play("Atk_Claw_L", 0, 0f);
+            break;
+            case 2:
+            GetComponent<Animator>().Play("Atk_Claw_R", 0, 0f);
+            break;
+            default:
+            break;
+        }
+
+        isAttacking = true;
+        agent.isStopped = true;
+      }
+    } else if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+    {
+        isAttacking = false;
+        agent.isStopped = false;
+    }
   }
 
 // This function subtracts damage from the dragon health
 // and despawns the dragon if all the heath is zero
+  public void TakeDamage(){
+    Debug.Log("TakeDamage() called");
+    dragonHealth = dragonHealth - swordDamage;
+
+    if (dragonHealth <= 0 && isDead == false) {
+      isDead = true;
+      Debug.Log("Dragon dying");
+      GetComponent<Animator>().Play("Dead_G", 0, 0f);
+      isDead = true;
+      agent.isStopped = true;
+      // Wait 5 seconds and then despawn the dragon
+      StartCoroutine(DragonDespawnWait());
+    }
+  }
+
+  // This function waits for 5 seconds and then despawns the dragon
+  private IEnumerator DragonDespawnWait(){
+    Debug.Log("Waiting for 5 seconds");
+    yield return new WaitForSecondsRealtime(5);
+    Debug.Log("Waited for 5 seconds");
+    Destroy(dragon);
+  }
 }
--- Unity Project/Assets/src/Corbin/DragonHead.cs
+++ Unity Project/Assets/src/Corbin/DragonHead.cs
@@ -10,15 +10,25 @@ using UnityEngine;
 
 public class DragonHead : MonoBehaviour
 {
-  // public GameObject dragon;
-  // void OnTriggerEnter (Collider col)
-  // {
-  //   Debug.Log("OnTriggerEnter() called");
-  //   if (col.gameObject.CompareTag("Sword"))
-  //   {
-  //     Debug.Log("Sword Dragon Collision detected");
-  //     dragon.GetComponent<DragonAI>().TakeDamage();
-  //     StartCoroutine(dragon.GetComponent<DragonAI>.DragonDespawnWait());
-  //   }
-  // }
+  public GameObject dragon;
+  private void Start()
+  {
+    dragon = GameObject.Find("DragonAI_2");
+  }
+  void OnTriggerEnter (Collider col)
+  {
+    Debug.Log("OnTriggerEnter() called");
+    if (col.gameObject.CompareTag("Sword"))
+    {
+      Debug.Log("Sword Dragon Collision detected");
+      dragon.GetComponent<DragonAI_2>().TakeDamage();
+      StartCoroutine(DragonDamageWait());
+    }
+  }
+
+  // This function waits for one second after the dragon takes
+  // damage so player cannot insta-kill dragon
+  private IEnumerator DragonDamageWait(){
+    yield return new WaitForSecondsRealtime(1);
+  }
 }
