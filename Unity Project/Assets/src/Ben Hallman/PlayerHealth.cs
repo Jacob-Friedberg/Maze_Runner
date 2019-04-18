475efc07bfae99b8604c7950b773d8e73b26ce2c
hallmanben <hallmanben@gmail.com>
2019-04-18 13:23:50 -0700

    Modualized Code

--- Unity Project/Assets/src/Ben Hallman/PlayerHealth.cs
+++ Unity Project/Assets/src/Ben Hallman/PlayerHealth.cs
@@ -12,9 +12,6 @@ public class PlayerHealth : MonoBehaviour
     // A boolean statement representing if the player is being damaged.
     private bool damaged;
 
-    // A static singleton property is used here as having more than one instance of these, might cause some very incorrect behavior.
-    public static ImageHandler Instance { get; private set; }
-
     // The amount of health the player starts the game with.
     public int startingHealth = 100;
     // The player's current health.
@@ -22,33 +19,16 @@ public class PlayerHealth : MonoBehaviour
     // The speed the damageImage will fade-out at.
     public float flashSpeed = 5f;
 
-    // Reference to an image that flashes on the screen when the player takes damage.
-    public Image damageImage;
     // Makes a GameObject called damageOverlay.
     public GameObject damageOverlay;
     // Adds the SoundManager GameObject.
     public GameObject SoundManager;
-
-    // The managing of colors below demonstrates the Prototype pattern in which new
-    // Color objects are created by copying pre-existing, selected Colors of the same type.
-    ColorManager colormanager = new ColorManager();
 
     // Reference to the player's movement.
     PlayerControl playerMovement;
 
     void Start()
     {
-        // Initialize with standard colors
-        colormanager["red"] = new Colors(255, 0, 0);
-        colormanager["green"] = new Colors(0, 255, 0);
-        colormanager["blue"] = new Colors(0, 0, 255);
-        colormanager["black"] = new Colors(0, 0, 0);
-        colormanager["white"] = new Colors(255, 255, 255);
-
-        // Clones selected colors
-        Colors red = colormanager["red"].Clone() as Colors;
-        Colors black = colormanager["black"].Clone() as Colors;
-
         // Adds the player heartbeat sound.
         AddSoundFromFile("myCoolSound", "folder/mySoundFile");
         // Adds the player damage sound.
@@ -62,9 +42,6 @@ public class PlayerHealth : MonoBehaviour
 
         // Sets the initial health of the player.
         currentHealth = startingHealth;
-
-        // Save a reference to the ImageHandler component as the singleton instance.
-        Instance = this;
     }
 
     void Update()
@@ -141,54 +118,5 @@ public class PlayerHealth : MonoBehaviour
             // Reset the initial health of the player.
             currentHealth = startingHealth;
         }
-    }
-
-    // Instance method, this method can be accesed through the singleton instance.
-    public void ImageColor(Colors color)
-    {
-        damageImage.color = color;
-    }
-}
-
-// This abstract class declares an interface for cloning itself
-abstract class ColorPrototype
-{
-    public abstract ColorPrototype Clone();
-}
-
-// This class implements an operation for cloning itself
-class Colors : ColorPrototype
-{
-    private int red;
-    private int green;
-    private int blue;
-
-    // This is the Colors constructor
-    public Colors(int red, int green, int blue)
-    {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-    }
-
-    // Creates a shallow copy
-    public override ColorPrototype Clone()
-    {
-        Debug.Log("CLONING COLOR RGB: {0,3},{1,3},{2,3}", red, green, blue);
-
-        return this.MemberwiseClone() as ColorPrototype;
-    }
-}
-
-// This class creates a new object by asking a prototype to clone itself
-class ColorManager
-{
-    private Dictionary<string, ColorPrototype> colors = new Dictionary<string, ColorPrototype>();
-
-    // The indexer
-    public ColorPrototype this[string key]
-    {
-        get { return colors[key]; }
-        set { colors.Add(key, value); }
     }
 }
